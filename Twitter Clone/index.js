4dd75125bdbfce7866966862265af4907f789180
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-10-17 13:12:00 +0500

    add images

--- Twitter Clone/index.js
+++ Twitter Clone/index.js
@@ -1,1 +1,9 @@
 import { tweetsData } from "./data";
+const tweetInput = document.getElementById("tweet-input")
+const tweetBtn = document.getElementById("tweet-btn")
+
+tweetBtn.addEventListener('click', function() {
+  console.log(tweetInput.value)
+})
+
+function 
