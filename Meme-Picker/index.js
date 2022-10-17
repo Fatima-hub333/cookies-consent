be5ea5a27fffd763775ae5686bed9e4ec245aa6e
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-10-17 11:14:47 +0500

    find matches with filter

--- Meme-Picker/index.js
+++ Meme-Picker/index.js
@@ -1,44 +1,101 @@
-const series = [
-  {
-      name: 'The Wire',
-      location: 'Baltimore',
-      lengthInHours : 60,
-      genres: ['action', 'thriller', 'detective', 'suspense']
-  },
-  {
-      name: 'Game of Thromes',
-      location: 'Westeros and Essos',
-      lengthInHours : 70.25,
-      genres: ['fantasy', 'action', 'tragedy']
-  },
-  {
-      name: 'Friends',
-      location: 'New York',
-      lengthInHours : 85,
-      genres: ['comedy', 'romance', 'drama']
-  },
-  {
-      name: 'The Walking Dead',
-      location: 'Atlanta',
-      lengthInHours : 131,
-      genres: ['zombie', 'apocalypse', 'thriller', 'suspense']
-  },
-  {
-      name: 'The Big Bang Theory',
-      location: 'Pasadena',
-      lengthInHours : 139.66,
-      genres: ['comedy', 'nerd', 'romance']
-  },
-]
+import { catsData } from '/data.js'
 
-const newYorkSeries = series.filter(function(show){
-    return show.location === 'New York'
-})
+const emotionRadios = document.getElementById('emotion-radios')
+const getImageBtn = document.getElementById('get-image-btn')
+const gifsOnlyOption = document.getElementById('gifs-only-option')
+const memeModalInner = document.getElementById('meme-modal-inner')
+const memeModal = document.getElementById('meme-modal')
+const memeModalCloseBtn = document.getElementById('meme-modal-close-btn')
 
-console.log(newYorkSeries)
+emotionRadios.addEventListener('change', highlightCheckedOption)
 
-const thrillers = series.filter(function (show) {
-  return show.genres.includes('thriller')
-})
+memeModalCloseBtn.addEventListener('click', closeModal)
 
-console.log(thrillers)
+getImageBtn.addEventListener('click', renderCat)
+
+function highlightCheckedOption(e){
+    const radios = document.getElementsByClassName('radio')
+    for (let radio of radios){
+        radio.classList.remove('highlight')
+    }
+    document.getElementById(e.target.id).parentElement.classList.add('highlight')
+}
+
+function closeModal(){
+    memeModal.style.display = 'none'
+}
+
+function renderCat(){
+    const catObject = getSingleCatObject()
+    memeModalInner.innerHTML =  `
+        <img 
+        class="cat-img" 
+        src="./images/${catObject.image}"
+        alt="${catObject.alt}"
+        >
+        `
+    memeModal.style.display = 'flex'
+}
+
+function getSingleCatObject(){
+    const catsArray = getMatchingCatsArray()
+    
+    if(catsArray.length === 1){
+        return catsArray[0]
+    }
+    else{
+        const randomNumber = Math.floor(Math.random() * catsArray.length)
+        return catsArray[randomNumber]
+    }
+}
+
+function getMatchingCatsArray(){     
+    if(document.querySelector('input[type="radio"]:checked')){
+        const selectedEmotion = document.querySelector('input[type="radio"]:checked').value
+        const isGif = gifsOnlyOption.checked
+        
+        const matchingCatsArray = catsData.filter(function(cat){
+            
+            if(isGif){
+                return cat.emotionTags.includes(selectedEmotion) && cat.isGif
+            }
+            else{
+                return cat.emotionTags.includes(selectedEmotion)
+            }            
+        })
+        return matchingCatsArray 
+    }  
+}
+
+function getEmotionsArray(cats){
+    const emotionsArray = []    
+    for (let cat of cats){
+        for (let emotion of cat.emotionTags){
+            if (!emotionsArray.includes(emotion)){
+                emotionsArray.push(emotion)
+            }
+        }
+    }
+    return emotionsArray
+}
+
+function renderEmotionsRadios(cats){
+        
+    let radioItems = ``
+    const emotions = getEmotionsArray(cats)
+    for (let emotion of emotions){
+        radioItems += `
+        <div class="radio">
+            <label for="${emotion}">${emotion}</label>
+            <input
+            type="radio"
+            id="${emotion}"
+            value="${emotion}"
+            name="emotions"
+            >
+        </div>`
+    }
+    emotionRadios.innerHTML = radioItems
+}
+
+renderEmotionsRadios(catsData)
