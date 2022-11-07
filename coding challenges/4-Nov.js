1057e9a0cdce64ba3b84d512b26c186cd856bd4b
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-11-07 21:38:20 +0500

    update the file

--- coding challenges/4-Nov.js
+++ coding challenges/4-Nov.js
@@ -188,3 +188,14 @@ const makeFlat = (array) => {
 }
 
 console.log(map([1, 2, 3], (v) => v + 1));
+
+// {Transoose Matrix}
+const transoseMatrix = (array) => {
+  return array[0].map((_, i)=> array.map(v => v[i]))
+}
+
+console.log(transoseMatrix([
+  [1, 2, 3],
+  [1, 2, 3],
+  [1, 2, 3],
+]))
