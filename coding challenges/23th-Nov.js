f19415fa523135d27b38dc9354a0785333a60ca8
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-11-23 11:54:36 +0500

    solved 2nd challenge

--- coding challenges/23th-Nov.js
+++ coding challenges/23th-Nov.js
@@ -13,3 +13,10 @@ this.removeDuplicates = function (head) {
 }
 
 // {Running Time & complexity}
+function processData(input) {
+  let arr = input.split('\n').map(v => parseInt(v)).slice(1);
+  for (const a of arr) {
+    const sqrt = Array(Math.floor(Math.sqrt(a)) - 1).fill(2).map((v, i) => v + i)
+    console.log(a === 1 || sqrt.some(i => a % i == 0) ? 'Not prime' : 'Prime')
+  }
+}
