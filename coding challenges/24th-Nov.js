20b13332acd193a61022d047903c3a44eb915224
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-11-24 11:35:05 +0500

    solve today challenge

---
+++ coding challenges/24th-Nov.js
@@ -0,0 +1,25 @@
+// {Nested Logic}
+function processData(input) {
+  let inputArr = input.split(" ");
+  let secondIndexElement = inputArr[2].split("\n");
+  let d1 = Number(inputArr[0]);
+  let m1 = Number(inputArr[1]);
+  let y1 = Number(secondIndexElement[0]);
+  let d2 = Number(secondIndexElement[1]);
+  let m2 = Number(inputArr[3]);
+  let y2 = Number(inputArr[4]);
+
+  let fine = 0;
+  if (d1 > d2 && m1 == m2 && y1 == y2) {
+    fine = (d1 - d2) * 15;
+  } else if (d1 < d2) {
+    fine = 0;
+  }
+  if (m1 > m2 && y1 == y2) {
+    fine = (m1 - m2) * 500;
+  }
+  if (y1 > y2) {
+    fine = 10000;
+  }
+  console.log(fine);
+}
