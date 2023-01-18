b9ed8f60b9ef5ec80cf36a4262718a466f0741bb
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2023-01-18 14:59:06 +0500

    add styled components in css style

--- styled/src/components/Content.js
+++ styled/src/components/Content.js
@@ -1,11 +1,37 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
-import ReactDOM  from 'react-dom'
+import ReactDOM from 'react-dom'
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  color: #aac9f0;
+`
+
+const Section = styled.div`
+  background-color: #f0d1aa;
+  flex-direction: column;
+  display: flex;
+  justify-content: center;
+  color: #f0aeaa;
+  border-radius: 15px;
+`
+
+const Button = styled.button`
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  padding: 5px;
+  margin: 5px;
+  border-radius: 15px;
+`
 
 const Content = () => {
   return (
-    <div className='section'>
-      <h1>Section</h1>
-    </div>
+    <Section className='section'>
+      <Title>💅 Section</Title>
+      <Button>Click me</Button>
+      <Button>Click me</Button>
+    </Section>
   )
 }
 
