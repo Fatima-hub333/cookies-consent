5fbfdc9bcbebce0afd7466262436ca36a9e93d18
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2023-01-19 15:05:14 +0500

    seperating components and passing propos

--- styled/src/index.js
+++ styled/src/index.js
@@ -1,73 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import App from './App';
-import './style.css'
-import Content from './components/Content'
+import React from 'react'
+import ReactDOM from 'react-dom'
 import styled from 'styled-components'
+import Section from './components/Section'
 
-// const Title = styled.h1`
-//   color: #b19cd9;
-// `
+const Title = styled.h1`
+  color: #b19cd9;
+`
 
-// const Section = styled.div`
-//   background-color: #ffffff;
-//   border: solid 3px grey;
-//   margin: 4px;
-//   width: 300px;
-//   height: 50px;
-//   border-radius: 10px;
-//   display: flex;
-//   justify-content: left;
-//   font-size: 40px;
-//   color: #ff6961;
-// `
-
-// const WeekdayTitle = styled.div`
-//   display: flex;
-//   justify-content: center;
-//   width: 50px;
-//   border-right: solid 3px lightgrey;
-// `
 
 class Main extends React.Component {
   render() {
     return (
       <>
-        <h1>Hello World!</h1>
-        <Content />
-        {/* <Title>Progress Tracker</Title>
+        <Title>Progress Tracker</Title>
         <div>
-          <Section>
-            <WeekdayTitle className='weekday-title'>M</WeekdayTitle>
-          </Section>
-
-          <Section>
-            <WeekdayTitle className='weekday-title'>T</WeekdayTitle>
-          </Section>
-
-          <Section>
-            <WeekdayTitle className='weekday-title'>W</WeekdayTitle>
-          </Section>
-
-          <Section>
-            <WeekdayTitle className='weekday-title'>T</WeekdayTitle>
-          </Section>
-
-          <Section>
-            <WeekdayTitle className='weekday-title'>F</WeekdayTitle>
-          </Section>
-
-          <Section>
-            <WeekdayTitle className='weekday-title'>S</WeekdayTitle>
-          </Section>
-
-          <Section>
-            <WeekdayTitle className='weekday-title'>S</WeekdayTitle>
-          </Section>
-        </div> */}
+          <Section text='M'/>
+          <Section text='T'/>
+          <Section text='W'/>
+          <Section text='T'/>
+          <Section text='F'/>
+          <Section text='S'/>
+          <Section text='S'/>
+        </div>
       </>
     );
   }
 }
 
-ReactDOM.render(<Main />, document.getElementById('root'));
+ReactDOM.render(<Main />, document.getElementById('root'))
