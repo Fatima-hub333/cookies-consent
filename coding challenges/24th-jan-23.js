030b06675d31ca1fd16c2491058e0eca90680a08
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2023-01-24 14:14:18 +0500

    solve panic function challenge

---
+++ coding challenges/24th-jan-23.js
@@ -0,0 +1,18 @@
+// Write a function that capitalizes a string
+
+let msg = 'hello hayya what are you doing?'
+
+function cap() {
+  return str.toUpperCase()
+}
+console.log(cap(msg))
+console.log(cap('take rest'))
+
+let message = 'hello'
+
+function capitalize() {
+  let capped = msg.toUpperCase()
+  console.log(capped)
+}
+
+capitalize();
