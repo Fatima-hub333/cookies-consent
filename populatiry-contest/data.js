12629cd40b669adcb46a7078541039f8db37f9de
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2023-02-03 12:53:16 +0500

    populatiry-content

---
+++ populatiry-contest/data.js
@@ -0,0 +1,27 @@
+export default [
+     {
+        post: "A lot of you have asked about my morning skin care routine...",
+        likes: 348
+     },
+     {
+        post: "New Year, new me. #blessed",
+        likes: 102
+     },
+     {
+        post: "Sunday by the pool 🌴 #vibes #mood #hotdoglegs",
+        likes: 544
+     },
+     {
+        post: "Here are my top 5 #notsponsored unbiased dry shampoo recs...",
+        likes: 745
+     },
+     {
+        post: "Last day in Hawaii! The tans will fade but the memories will last forever #holidays #foreverhawaii #blessed #followme",
+        likes: 412
+     },
+     {
+        post: "Leg day #summerbody2022 #gymshark #beachworkout",
+        likes: 275
+     },
+]
+   
